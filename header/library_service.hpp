6b3a75c4268c4d18175ed18e8fa0daa20b1a46cb
Aydan <akkzacam@yahoo.com>
2025-12-21 23:03:35 +0800

    added library services funcationly; incomplete

--- header/library_service.hpp
+++ header/library_service.hpp
@@ -2,16 +2,35 @@
 #define LIBRARY_SERVICE_HPP
 
 #include <string>
+#include <vector>
+
+//temp book file 
+struct Book
+{
+    std::string id;
+    std::string title;
+    std::string author;
+    std::string genre;
+    bool is_borrowed;
+};
 
 class LibraryService
 {
+    private:
+    std::vector<Book> books; //temp
+
     public:
+    LibraryService(); //temp
+
+    // guest + member + librarian
     void searchByTitle();
     void searchByAuthor();
     void searchByGenre();
 
+    //member + librarian
     bool borrowBook();
     bool returnBook();
+    void checkBorrowed();
 
     //librarian only features;
     bool addBook();
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <limits>
 #include <vector> //temporary for storing user files
 #include "../header/people_database_temp.hpp"
 #include "../header/person.hpp"
@@ -57,17 +58,30 @@ TempUser* login(std::vector<TempUser>& users)
 
 //user menu
 
-void guestMenu(const TempUser& user)
+void guestMenu(const TempUser& user, LibraryService& library)
 {
     int choice;
     do
     {
         std::cout << "guest menu" << std::endl;
-        std::cout << "1. search book" << std::endl;
+        std::cout << "1. search book by title" << std::endl;
+        std::cout << "2. search book by author" << std::endl;
+        std::cout << "3. search book by genre" << std::endl;
         std::cin >> choice;
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 
         if (choice == 1)
-        {}
+        {
+            library.searchByTitle();
+        }
+        else if (choice == 2)
+        {
+            library.searchByAuthor();
+        }
+        else if (choice == 3)
+        {
+            library.searchByGenre();
+        }
         else if (choice == 0)
         {
             break;
@@ -75,25 +89,45 @@ void guestMenu(const TempUser& user)
     } while (choice != 0);
 }
 
-void memberMenu(const TempUser& user)
+void memberMenu(const TempUser& user, LibraryService& library)
 {
     int choice;
     do
     {
         std::cout << "\nWelcome, Member: " << user.name << std::endl;
         std::cout << "Allowed actions:" << std::endl;
-        std::cout << "1. Search books" << std::endl;
-        std::cout << "2. Borrow book" << std::endl;
-        std::cout << "3. Return book" << std::endl;
-        std::cout << "4. View borrowed books" << std::endl;
+        std::cout << "1. search book by title" << std::endl;
+        std::cout << "2. search book by author" << std::endl;
+        std::cout << "3. search book by genre" << std::endl;
+        std::cout << "4. Borrow book" << std::endl;
+        std::cout << "5. Return book" << std::endl;
+        std::cout << "6. View borrowed books" << std::endl;
         std::cin >> choice;
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 
         if (choice == 1)
-        {}
+        {
+            library.searchByTitle();
+        }
         else if (choice == 2)
-        {}
+        {
+            library.searchByAuthor();
+        }
         else if (choice == 3)
-        {}
+        {
+            library.searchByGenre();
+        }
+        else if (choice == 4)
+        {
+            library.borrowBook();
+        }
+        else if (choice == 5)
+        {
+            library.returnBook();
+        }
+        else if (choice == 6) //forgot lmao
+        {
+        }
         else if (choice == 0)
         {
             break;
@@ -104,41 +138,71 @@ void memberMenu(const TempUser& user)
     return;
 }
 
-void librarianMenu(const TempUser& user)
+void librarianMenu(const TempUser& user, LibraryService& library)
 {
     int choice;
     do
     {
         std::cout << "\nWelcome, librarian: " << user.name << std::endl;
-        std::cout << "1. Add book" << std::endl;
-        std::cout << "2. Remove book" << std::endl;
-        std::cout << "3. Register member" << std::endl;
-        std::cout << "4. Remove member" << std::endl;
-        std::cout << "5. Borrow book" << std::endl;
-        std::cout << "6. Return book" << std::endl;
-        std::cout << "7. Fine calculation" << std::endl;
+        std::cout << "1. search book by title" << std::endl;
+        std::cout << "2. search book by author" << std::endl;
+        std::cout << "3. search book by genre" << std::endl;
+        std::cout << "4. Borrow book" << std::endl;
+        std::cout << "5. Return book" << std::endl;
+        std::cout << "6. Add book" << std::endl;
+        std::cout << "7. Remove book" << std::endl;
+        std::cout << "8. Register member" << std::endl;
+        std::cout << "9. Remove member" << std::endl;
+        std::cout << "10. Fine calculation" << std::endl;
         std::cin >> choice;
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 
         if (choice == 1)
-        {}
+        {
+            library.searchByTitle();
+        }
         else if (choice == 2)
-        {}
+        {
+            library.searchByAuthor();
+        }
         else if (choice == 3)
-        {}
+        {
+            library.searchByGenre();
+        }
         else if (choice == 4)
-        {}
+        {
+            library.borrowBook();
+        }
         else if (choice == 5)
-        {}
-        else if (choice == 6)
-        {}
+        {
+            library.returnBook();
+        }
+        /*
+        else if (choice == 6) //forgot lmao
+        {
+        }
         else if (choice == 7)
-        {}
+        {
+            library.removeBook();
+        }
+        else if (choice == 8)
+        {
+            library.registerMember();
+        }
+        else if (choice == 9)
+        {
+            library.removeMember();
+        }
+        else if (choice == 10)
+        {
+            library.calculateFine();
+        }
+        */
         else if (choice == 0)
         {
             break;
         }
     } while (choice != 0);
-
 }
 
 int main()
@@ -155,18 +219,20 @@ int main()
         return 0;
     }
 
+    LibraryService library;
+
     //leads users to respective menu, from there they can do actions within the system
     if (currentUser->role == "Guest")
     {
-        guestMenu(*currentUser);
+        guestMenu(*currentUser, library);
     }
     else if (currentUser->role == "Member")
     {
-        memberMenu(*currentUser);
+        memberMenu(*currentUser, library);
     }
     else if (currentUser->role == "Librarian")
     {
-        librarianMenu(*currentUser);
+        librarianMenu(*currentUser, library);
     }
 
     return 0;
