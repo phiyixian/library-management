456ff92d475e5b782c4e5a1aa5735430f82abadf
drdoof3 <somefangirl666@gmail.com>
2026-01-20 18:17:22 +0800

    nudge guest to log in

--- src/guest.cpp
+++ src/guest.cpp
@@ -33,6 +33,7 @@ void Guest::showMenu(LibraryService &library)
         std::cout << "  3. Search by Genre" << std::endl;
         std::cout << "  4. Display Full Catalogue" << std::endl;
         std::cout << "\n  0. Exit" << std::endl;
+        std::cout << "\nIf you would like to borrow a book, please exit and sign in as a member! :)" << std::endl;
         std::cout << std::string(50, '-') << std::endl;
         std::cout << "Enter your choice: ";
         std::cin >> choice;
