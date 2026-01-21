8c6c593490cf950489561b918fae0bc737366987
Aydan <akkzacam@yahoo.com>
2026-01-21 16:01:58 +0800

    after exiting meny, returned to library management system menu

--- src/main.cpp
+++ src/main.cpp
@@ -40,111 +40,117 @@ int main()
     Person *user = nullptr;
     LibraryService library;
 
-    clearScreen();
-
-    /*printHeader("Library Management System");
-    std::cout << "\n1. Register (New Member)" << std::endl;
-    std::cout << "2. Login" << std::endl;
-    std::cout << "3. Continue as Guest" << std::endl;
-    std::cout << "0. Exit" << std::endl;
-    printSeparator();*/
-
-    std::cout << R"(
-
-    _____
-   /    /|_ ___________________________________
-  /    // /|       ~~*~~                      /|
- (====|/ //  Welcome to the...   ______      / |
-  (=====|/    SMART LIBRARY     /  /  / |   / .|
- (====|/    MANAGEMENT SYSTEM  /__/__/ (_) / /||
-/_________________________________________/ / ||
-|  _____________________________________  ||  ||
-| ||                                    | ||
-| ||                                    | ||
-| ||                                    | || 
-)" << std::endl;
-
-std::cout << "\nPress Enter to log in..." << std::endl;
-std::cin.get();
-clearScreen();
-
-std::cout << R"(
-      _.--._  _.--._             -----~~~~====*====~~~~-----
-,-=.-":;:;:;\':;:;:;"-._              ˗ˏˋ ꒰ SLMP  ꒱ˎˊ˗
-\\\:;:;:;:;:;\:;:;:;:;:;\         
- \\\:;:;:;:;:;\:;:;:;:;:;\        1. Register as New Member
-  \\\:;:;:;:;:;\:;:;:;:;:;\       2. Login
-   \\\:;:;:;:;:;\:;::;:;:;:\      3. Continue as Guest
-    \\\;:;::;:;:;\:;:;:;::;:\     0. Exit
-     \\\;;:;:_:--:\:_:--:_;:;\   -----~~~~====*====~~~~----- 
-      \\\_.-"      :      "-._\
-       \`_..--""--.;.--""--.._=>    
-
-)" << std::endl;
-
-    std::cout << "\nEnter your choice: ";
-    std::cin >> choice;
-
-    switch (choice)
+    while (true)
     {
-    case 1: // Register
-        printHeader("Member Registration");
-        user = registerUser("MEMBER");
+        clearScreen();
+
+        std::cout << R"(
+
+            _____
+        /    /|_ ___________________________________
+        /    // /|       ~~*~~                      /|
+        (====|/ //  Welcome to the...   ______      / |
+        (=====|/    SMART LIBRARY     /  /  / |   / .|
+        (====|/    MANAGEMENT SYSTEM  /__/__/ (_) / /||
+        /_________________________________________/ / ||
+        |  _____________________________________  ||  ||
+        | ||                                    | ||
+        | ||                                    | ||
+        | ||                                    | || 
+        )" << std::endl;
+
+        std::cout << "\nPress Enter to log in..." << std::endl;
+        std::cin.get();
+        clearScreen();
+
+        std::cout << R"(
+                _.--._  _.--._             -----~~~~====*====~~~~-----
+            ,-=.-":;:;:;\':;:;:;"-._              ˗ˏˋ ꒰ SLMP  ꒱ˎˊ˗
+            \\\:;:;:;:;:;\:;:;:;:;:;\         
+            \\\:;:;:;:;:;\:;:;:;:;:;\        1. Register as New Member
+            \\\:;:;:;:;:;\:;:;:;:;:;\       2. Login
+            \\\:;:;:;:;:;\:;::;:;:;:\      3. Continue as Guest
+            \\\;:;::;:;:;\:;:;:;::;:\     0. Exit
+            \\\;;:;:_:--:\:_:--:_;:;\   -----~~~~====*====~~~~----- 
+            \\\_.-"      :      "-._\
+            \`_..--""--.;.--""--.._=>    
+
+        )" << std::endl;
+
+        std::cout << "\nEnter your choice: ";
+        std::cin >> choice;
+
+        if (std::cin.fail())
+        {
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            std::cout << "\n[ERROR] Invalid option. Please choose from (1-3), or 0 to exit." << std::endl;
+            continue;
+        }
+
+        switch (choice)
+        {
+        case 1: // Register
+            printHeader("Member Registration");
+            user = registerUser("MEMBER");
+            if (user)
+            {
+                std::cout << "\n[SUCCESS] Registration successful!\n";
+            }
+            else
+            {
+                std::cout << "\n[ERROR] Registration failed. Please try again.\n";
+            }
+            break;
+
+        case 2: // Login
+            printHeader("Login");
+            do 
+            {
+                user = login();
+                if (user == nullptr)
+                {
+                    std::cout << "\n[ERROR] User ID not found or Username mismatch. Try again." << std::endl;
+                }
+            } while(user == nullptr);
+            
+            std::cout << "\n[SUCCESS] Login successful!\n";
+            break;
+
+        case 3: // Continue as Guest
+            {
+            printHeader("Guest Access");
+            std::string guestname, guestemail;
+            std::cin.ignore();
+            std::cout << "Enter Your Name: ";
+            std::getline(std::cin, guestname);
+            //std::cout << "Enter Your Email: ";
+            //std::getline(std::cin, guestemail);
+            
+            user = new Guest("GXXX", guestname, guestemail);
+            std::cout << "\n[SUCCESS] Continuing as Guest!\n";
+            break;
+            }
+
+        case 0:
+            std::cout << "\nThank you for using Library Management System. Goodbye!\n";
+            return 0;
+
+        default:
+            continue;
+        }
+
         if (user)
         {
-            std::cout << "\n[SUCCESS] Registration successful!\n";
+            user->showMenu(library);
+            delete user; // Clean up
         }
         else
         {
-            std::cout << "\n[ERROR] Registration failed. Please try again.\n";
+            std::cout << "\n[ERROR] No user session started.\n";
         }
-        break;
-
-    case 2: // Login
-        printHeader("Login");
-        do 
-        {
-            user = login();
-            if (user == nullptr)
-            {
-                std::cout << "\n[ERROR] User ID not found or Username mismatch. Try again." << std::endl;
-            }
-        } while(user == nullptr);
-        
-        std::cout << "\n[SUCCESS] Login successful!\n";
-        break;
-
-    case 3: // Continue as Guest
-        {
-        printHeader("Guest Access");
-        std::string guestname, guestemail;
-        std::cin.ignore();
-        std::cout << "Enter Your Name: ";
-        std::getline(std::cin, guestname);
-        //std::cout << "Enter Your Email: ";
-        //std::getline(std::cin, guestemail);
-        
-        user = new Guest("GXXX", guestname, guestemail);
-        std::cout << "\n[SUCCESS] Continuing as Guest!\n";
-        break;
-        }
-    case 0:
-        std::cout << "\nThank you for using Library Management System. Goodbye!\n";
-        return 0;
-    default:
-        std::cout << "\n[ERROR] Invalid option. Please choose from (1-3), or 0 to exit." << std::endl;
-        return 0;
     }
 
-    if (user)
-    {
-        user->showMenu(library);
-        delete user; // Clean up
-    }
-    else
-    {
-        std::cout << "\n[ERROR] No user session started.\n";
-    }
 
     return 0;
 }
